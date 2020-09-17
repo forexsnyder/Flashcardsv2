e38cc31786d983f21a8168e404c3be221da141f6
forexsnyder <forexsnyder@gmail.com>
2020-09-17 11:30:57 -0700

    database fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :flavors, only: :index
+  resources :foods
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users, only: :create
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  
-  
+  get '/foods/:food_id/flavors/:id', to: 'flavors#add_flavor_to_food'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_164151) do
+ActiveRecord::Schema.define(version: 2020_09_11_182102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flashcard_set", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "flashcard_set_flashcards", id: false, force: :cascade do |t|
+    t.bigint "flashcards_id", null: false
+    t.bigint "flashcard_sets_id", null: false
+  end
+
+  create_table "flashcards", force: :cascade do |t|
+    t.string "front"
+    t.text "back"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_flashcards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_09_16_164151) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "flashcards", "users"
 end
